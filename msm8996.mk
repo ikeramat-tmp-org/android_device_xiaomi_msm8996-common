bb6595cdd4c1391472d8e00fdc44ad7e25753082
iKeramat <ikeramat80@gmail.com>
2020-07-05 13:11:00 +0430

    msm8996-common: Remove LineageOS stuff
    * [SQUASH] msm8996-common: configpanel: Port to AOSP https://github.com/Project-Treble-Mi5/android_device_xiaomi_msm8996-common/commit/a348dbe538f0161369127428d9fb8ed58ac77eb8#diff-2606c2f0888d8a147a13ffd9ac15d9b7
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -23,19 +23,13 @@ $(call inherit-product, vendor/xiaomi/msm8996-common/msm8996-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-extended
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -250,10 +244,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_8996
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -374,11 +364,7 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/msm8996-common \
     hardware/xiaomi
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
+    
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
